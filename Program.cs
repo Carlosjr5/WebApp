d841d12a9ea72f761ed6939fb74cc98c54984a39
Carlos Jimenez Rodriguez <77840319+Carlosjr5@users.noreply.github.com>
2023-09-17 20:49:44 +0100

    Data Transfer Objects -- DTOs
    - Getting All data from SQL Database.
    - Retrieving data by ID.
    - Adding data to Database.
    - Updating Data.
    - Deleting Data.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Database Injection
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
+
+
+builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
